13377d3d35da3cb4a5e58602eb97dad71c8a4e34
zizooseesaw <palossherlyn@gmail.com>
2025-04-01 18:08:54 -0700

    cambios en AnimalsShow

---
+++ src/AnimalShow.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+
+import bird from './assets/svg/bird.svg'
+import dog from './assets/svg/dog.svg'
+import cat from './assets/svg/cat.svg'
+import cow from './assets/svg/cow.svg'
+import gator from './assets/svg/gator.svg'
+import heart from './assets/svg/heart.svg'
+import horse from './assets/svg/horse.svg'
+
+import './AnimalsShow.css'
+
+const svgMap = {
+    bird,
+    dog,
+    cat,
+    cow,
+    gator,
+    horse
+}
+
+function AnimalsShow{(type)}{
+    const [clicks, setClicks] = useState(0)
+
+    const handleClick = () => {
+        setClicks(clicks + 1)
+    }
+
+    return(
+        <div
+            onClick={handleClick}
+            className="animals-show"
+        >
+            <img
+                src={svgMap[type]}
+                alt="animal"
+                className="animal"
+            />
+            <img
+                src={heart}
+                alt="heart"
+                style={{ width: 10 + 10 * clicks + 'px'}}
+                className="heart"
+            />
+        </div>    
+    )
+}
+
+export default AnimalsShow
